462427e93351f5f39df6e04213ac24c2a41c8ac9
youssef jeb <jebbouriyoussef@gmail.com>
2019-04-11 14:37:14 +0100

    Article adding fonction

--- Article/Article.cpp
+++ Article/Article.cpp
@@ -13,31 +13,69 @@ typedef struct Article{
 	
 	
 }Article;
-Article * init_list(Article* a){
+Article * init_Article(Article* a){
 	
 	a = NULL;
 	return a;
 }
 
 
- Article * add_Article(Article *a , char* nv_art, int num, int tele, int id, int prix){
- 
+ Article * add_Article(Article *a ){
+  
  Article * nv_article = (Article*)malloc(sizeof (Article));
  if (nv_article == NULL) return 0; 
  
  nv_article->nom_clt =(char*) malloc(sizeof(char));
  if (nv_article->nom_clt == NULL) return 0;
  
- a->suivant = nv_article;
- strcpy(nv_article->nom_clt,a->nom_clt);
+  nv_article->suivant = a;
+  
+  
+ printf("entre le nom du client");
+ scanf("%s",&nv_article->nom_clt);
  
- nv_article->id_article = id;
- nv_article->number = a->number;
- nv_article->prix = prix;
+  
+ printf("entre le numero : ");
+ scanf("%d",&nv_article->number);
+ 
+  
+ printf("entre le id de la piece : ");
+ scanf("%d",&nv_article->id_article);
+ 
+  
+ printf("entre le prix de la piece : ");
+ scanf("%d",&nv_article->prix);
+ 
+ 
+
+ 
+ 
+ 
+ FILE * f ;
+ f = fopen("produit_ajouter.txt","w+");
+ 
+ 	if(f == NULL) return 0;
+ 	printf("\n confirme\n");
+ 	fprintf(f,"%s\n%d\n%d\n%d\n",nv_article->nom_clt,nv_article->number,nv_article->id_article,nv_article->prix);
+ 	printf("\n confirme\n");
+ 	fclose(f);
  
  
  
  
+ return nv_article;
+ 
+ }
+ 
+ 
+ 
+ main(){
+ 	
+ 	Article *a = (Article*)malloc(sizeof(Article));
+ 	init_Article(a);
+ 	add_Article(a);
+ 	
+ 	
  }
 
 
