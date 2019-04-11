b68c07fcd5be0d182acca632eb819eb2641ec8ef
youssef jeb <jebbouriyoussef@gmail.com>
2019-04-11 15:44:22 +0100

    Article adding fonction & go to repare function

--- Article/Article.cpp
+++ Article/Article.cpp
@@ -1,12 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
-
+// Article structur declaration with the add function 
 typedef struct Article{
 	
 	int id_article;
 	int number;
-	char *nom_clt;
+	char *nom_art;
 	int prix;
 	
 	struct Article *suivant;
@@ -18,21 +18,19 @@ Article * init_Article(Article* a){
 	a = NULL;
 	return a;
 }
-
-
  Article * add_Article(Article *a ){
   
  Article * nv_article = (Article*)malloc(sizeof (Article));
  if (nv_article == NULL) return 0; 
  
- nv_article->nom_clt =(char*) malloc(sizeof(char));
- if (nv_article->nom_clt == NULL) return 0;
+ nv_article->nom_art =(char*) malloc(sizeof(char));
+ if (nv_article->nom_art == NULL) return 0;
  
   nv_article->suivant = a;
   
   
- printf("entre le nom du client");
- scanf("%s",&nv_article->nom_clt);
+ printf("entre le nom du piece");
+ scanf("%s",nv_article->nom_art);
  
   
  printf("entre le numero : ");
@@ -52,20 +50,46 @@ Article * init_Article(Article* a){
  
  
  FILE * f ;
- f = fopen("produit_ajouter.txt","w+");
+ f = fopen("produit_ajouter.txt","a+");
  
  	if(f == NULL) return 0;
- 	printf("\n confirme\n");
- 	fprintf(f,"%s\n%d\n%d\n%d\n",nv_article->nom_clt,nv_article->number,nv_article->id_article,nv_article->prix);
- 	printf("\n confirme\n");
+ 
+ 	
+ 	fprintf(f,"\n%d\n%d\n%d\n%s\n",nv_article->prix,nv_article->number,nv_article->id_article,nv_article->nom_art);
+ 	
+ 
  	fclose(f);
- 
- 
- 
  
  return nv_article;
  
  }
+ 
+ Article * go_to_repare(Article *a){
+ FILE *g= fopen("go_to_repare.txt","r+");
+ fprintf(g,"%d\n",a->id_article);
+ }
+ 
+ 
+ // repared Articles :  stac structure repair article function
+ 
+ typedef struct Article_repare{
+int id_Article;
+struct Article_repare *suivant;
+}Article_repare;
+
+typedef struct pile{
+
+Article *sommet;
+int taille;
+int Article_id;
+}pile;
+
+void init_pile(pile *p){
+p->sommet=NULL;
+p->taille =0;
+
+}
+ 
  
  
  
