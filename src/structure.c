d67eba75ee9c5fb4f6cbd2d029fb6b1772438ff9
Othmane Mdarhri <outhman.mdarhri@gmail.com>
2019-04-11 03:05:21 +0100

    ajout de structure etat et date et job

--- src/structure.c
+++ src/structure.c
@@ -1,16 +1,25 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
 
+typedef struct {
+    int j,m,a;
+}date;
+typedef enum etat{
+    RECU,EN_DIAG,REP,DEP
+}etat;
+typedef struct {
+    int id,id_tech;
+    char nom_client[30],desc[50];
+    date d_rec,d_rep;
+    etat et_job;
+}job;
 
-typedef struct Article {
+typedef struct article {
 
     int id_article;
     char nom_cl[30];
     int num_tele;
     int prix;
 
-    } Article;
+    } article;
 
 typedef struct {
     int id;
